c2f3c25e3047ef41576d834e40067b3b62323b37
arth3rs0ng@gmail.com <arthursong14@gmail.com>
2021-03-31 15:50:38 -0700

    add favicon and app title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,16 @@
 // import '../styles/globals.css'
 import 'tailwindcss/tailwind.css'
 import type { AppProps /*, AppContext */ } from 'next/app'
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+  <Head>
+    <link rel="shortcut icon" href="/favicons/favicon.ico" /> 
+    <title>Learn or Die</title>
+  </Head>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
