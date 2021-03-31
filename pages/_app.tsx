747e5b3aa63e44de76f1c636c70c8df9db633aac
arth3rs0ng@gmail.com <arthursong14@gmail.com>
2021-03-31 16:00:39 -0700

    add script for google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,22 @@ function MyApp({ Component, pageProps }: AppProps) {
   <Head>
     <link rel="shortcut icon" href="/favicons/favicon.ico" /> 
     <title>Learn or Die</title>
+
+    {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-J45C8054NT"></script>
+
+    <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-J45C8054NT', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
   </Head>
     <Component {...pageProps} />
   </>
