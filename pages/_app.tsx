b55a60b83040d326da4338cf3e743feab483f513
arth3rs0ng@gmail.com <arthursong14@gmail.com>
2021-04-15 18:00:18 -0700

    reorganize components outside pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import type { AppProps /*, AppContext */ } from 'next/app'
 import React from 'react'
 import {MDXProvider} from '@mdx-js/react'
 import Head from 'next/head';
-import { H1, H2, Code } from './components/Mardown';
+import { H1, H2, Code } from '../components/Markdown';
 
 
 const components = {
