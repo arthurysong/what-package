b920da84dcf7e94264f9954f0c2afc9ad3819a8f
arth3rs0ng@gmail.com <arthursong14@gmail.com>
2021-04-20 14:53:58 -0700

    index page with links to my posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,8 @@
 import React, { FunctionComponent } from 'react';
 
 import Link from 'next/link'
-// import next, { GetServerSideProps } from 'next'
-// import axios from 'axios'
-// import moment from 'moment'
-// // import Chart from 'chart.js';
-// import { Line } from 'react-chartjs-2';
-// import useSWR from 'swr';
-
 interface Props {
-  // data: {
-  //   downloadsEachWeek: Array<number>,
-  //   labels: Array<string>,
-  //   avgGrowthOverWeek: number
-  // }
+  
 }
 
 const API_URL = "https://arthurblogsapi.com/api/blogs";
@@ -22,6 +11,16 @@ const Home = ( props: Props ) => {
 
   return <div className="min-h-screen bg-gray-800">
     <div className="relative min-h-screen max-w-screen-sm m-auto text-white flex flex-col items-start p-8">
+      <header className="flex flex-col items-center">
+        <h1 className="p-8 text-4xl my-12">
+          What %&!@ing Technology do I need to learn now?
+        </h1>
+      </header>
+      <ul className="list-disc">
+      <li><Link href="/chatter" ><a className="underline">Chatter: Scaling Stateful Websockets Elixir app horizontally (AWS) 
+        <span className="text-sm">You suck at scaling and systems design**</span></a></Link></li>
+      <li><Link href="/chatter" ><a className="underline">What %&!@ing Technology do I need to learn now? (A joke, Javascript/NPM)</a></Link></li>
+      </ul>
     </div>
   </div>
 }
